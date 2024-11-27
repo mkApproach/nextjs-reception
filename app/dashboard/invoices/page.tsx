f772bc70c24db3c719bcb774337b4b75e5b15a21
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-11-27 17:33:00 +0900

    firstCommit

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -35,10 +35,10 @@ export default async function Page(
     return (
         <div className="w-full">
           <div className="flex w-full items-center justify-between">
-            <h1 className={`${lusitana.className} text-2xl`}>請求書一覧</h1>
+            <h1 className={`${lusitana.className} text-2xl`}>受付一覧</h1>
           </div>
           <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-            <Search placeholder="請求書の検索..." />
+            <Search placeholder="受付の検索..." />
             <CreateInvoice />
           </div>
           <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
