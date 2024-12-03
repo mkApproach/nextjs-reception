bdc92e097ff6fc7b8cd955eca8c897581688249e
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-03 19:17:00 +0900

    firstCommit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 const authOptions: NextAuthOptions = {
+ secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: 'Ninjin Sirisiri',
