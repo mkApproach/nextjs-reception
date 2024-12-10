bf17c220ff280cbfa6202bf3c2b8af91de7d1ede
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-10 17:26:19 +0900

    firstCommit

--- src/app/ui/acme-logo.tsx
+++ src/app/ui/acme-logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-8 w-8 rotate-[10deg]" />
-      <p className="text-[16px]">大会受付</p>
+      <p className="text-[16px]">大会の受付</p>
     </div>
   );
 }
