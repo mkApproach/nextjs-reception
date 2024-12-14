59e508982544e8aef5a89c5204a08884e7d431ae
mkApproach <kinai-ap@cameo.plala.or.jp>
2024-12-14 18:25:27 +0900

    firstCommit

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -9,7 +9,7 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex flex-col justify-center h-20 items-end  rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex flex-col justify-start h-20 items-end  rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
